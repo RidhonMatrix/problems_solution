a72f55099a583a924faf92346c75bc8e5894ebc0
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-01-09 21:06:13 +0600

    Update linkList.cpp

--- Template/DataStructure/linkList.cpp
+++ Template/DataStructure/linkList.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+//Node* temp = (Node*) malloc(sizeof(Node));
+
 class Node{ 
 public:  
 	int data; 
@@ -25,3 +27,44 @@ void deleteList(Node** head){
 	*head = NULL;  
 } 
 
+void push(Node** head, int new_data){ 
+    Node* new_node = (Node*) malloc(sizeof(Node)); 
+    new_node->data  = new_data; 
+    new_node->next = (*head); 
+    (*head)    = new_node; 
+} 
+
+void deleteNode(Node **head, int position){ 
+   if (*head == NULL) 
+      return;
+   Node* temp = *head;   
+    if (position == 0) { 
+        *head = temp->next;   
+        free(temp);
+        return; 
+    }  
+    for (int i=0; temp!=NULL && i<position-1; i++) 
+         temp = temp->next;   
+    if (temp == NULL || temp->next == NULL) 
+         return; 
+    Node *next = temp->next->next; 
+    free(temp->next); 
+    temp->next = next; 
+} 
+
+int main() 
+{ 
+	Node* head = NULL; 
+	push(&head, 7); 
+	push(&head, 1); 
+	push(&head, 3); 
+	push(&head, 2); 
+		
+	printList(head);
+
+	deleteNode(&head, 1); 
+	
+	printList(head); 
+	
+	return 0; 
+} 
