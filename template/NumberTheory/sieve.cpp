205393db67c491d27322e38c0d0dcbfd08c17000
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-01-10 17:21:01 +0600

    Create sieve.cpp

---
+++ template/NumberTheory/sieve.cpp
@@ -0,0 +1,16 @@
+bool marked[1005];
+bool isPrime(int n){
+	if(n<2)    return false;
+	if(n==2)   return true;
+	if(n%2==0) return false;
+	return marked[n]==false; 
+}
+void sieve(int n){
+	for(int i=3;i*i<=n;i+=2){
+		if(marked[i]==false){
+			for(int j=i*i;j<=n;j+=i+i){
+				marked[j]=true;
+			}
+		}
+	}
+}
