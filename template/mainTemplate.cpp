d43990b3f687f9fb4483773787a671626ef24692
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-05-06 22:58:17 +0600

    Update mainTemplate.cpp

--- template/mainTemplate.cpp
+++ template/mainTemplate.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define F            first
+#define S            second  
+#define Fin          freopen("input.txt","r",stdin)
+#define Fout         freopen("output.txt","w",stdout)
+#define FasterIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
 #define for0(i, n) for (int i = 0; i < (int)(n); ++i)
 
 void solve(){
@@ -8,8 +14,7 @@ void solve(){
 }
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    FasterIO;
     int tc = 1;
     //cin >> tc;
     for (int t = 1; t <= tc; t++) {
