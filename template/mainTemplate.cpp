f385bdb0d78914fac494fec28e08eb6728923d98
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-05-06 22:55:20 +0600

    Update mainTemplate.cpp

--- template/mainTemplate.cpp
+++ template/mainTemplate.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)
+
+void solve(){
+    
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-//    freopen("in.txt","r",stdin);
-//    freopen("out.txt","w",stdout);
-    
-  
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        //cout << "Case #" << t  << ": ";
+        solve();
+    }
 }
