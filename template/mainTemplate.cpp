25ff9e34c117022f91b970cc7165080082259cc6
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-05-06 22:29:27 +0600

    Update mainTemplate.cpp

--- template/mainTemplate.cpp
+++ template/mainTemplate.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 //    freopen("in.txt","r",stdin);
 //    freopen("out.txt","w",stdout);
-  
+    
   
 }
