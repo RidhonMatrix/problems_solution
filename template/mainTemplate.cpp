deb743ee28f2601154c7c31fd2fddc59862a8e17
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-05-07 14:31:25 +0600

    Update mainTemplate.cpp

--- template/mainTemplate.cpp
+++ template/mainTemplate.cpp
@@ -3,18 +3,24 @@ using namespace std;
 
 #define F            first
 #define S            second  
-#define Fin          freopen("input.txt","r",stdin)
-#define Fout         freopen("output.txt","w",stdout)
+#define Fin          freopen("input.txt","r",stdin);
+#define Fout         freopen("output.txt","w",stdout);
 #define FasterIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
 #define for0(i, n) for (int i = 0; i < (int)(n); ++i)
+
+template <typename T> void Print(const vector<T> &v) {for (int i = 0; i + 1 < v.size() ; i++) cout << v[i] << " ";cout << v.back() << "\n";}
 
 void solve(){
     
 }
 int main()
 {
-    FasterIO
+    #ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif
+    
     int tc = 1;
     //cin >> tc;
     for (int t = 1; t <= tc; t++) {
