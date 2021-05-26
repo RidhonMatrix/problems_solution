32da7348828d7c96b18f0067cc4bb5f6bb98a946
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-05-27 04:35:36 +0600

    Create 10220-I_Love_Big_Numbers.cpp

---
+++ uva/10220-I_Love_Big_Numbers.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI acos(-1)
+void multiply(int x,vector<int> &res){
+    int carry=0;
+    for(int i=0;i<res.size();i++){
+        int prod=res[i]*x+carry;
+        res[i]=prod%10;
+        carry=prod/10;
+    }
+    while(carry){
+        res.push_back(carry%10);
+        carry/=10;
+    }
+
+}
+void factorial(int n){
+    vector<int> res;
+    res.push_back(1);
+
+    for(int i=2;i<=n;i++){
+        multiply(i,res);
+    }
+    int sum=0;
+    for(int i=0;i<res.size();i++){
+        sum+=res[i];
+    }
+    cout<<sum<<'\n';
+}
+int main()
+{
+    int n;
+    while(cin>>n){
+        factorial(n);
+    }
+}
