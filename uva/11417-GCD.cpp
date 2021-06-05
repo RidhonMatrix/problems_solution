5c9bc9cfed048be0bf2153f10500f32cde044d4f
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-06-05 23:04:19 +0600

    Create 11417-GCD.cpp

---
+++ uva/11417-GCD.cpp
@@ -0,0 +1,48 @@
+/***
+**    Author: Rejaul Karim Ridhon
+***/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll           long long
+
+#define pb           push_back
+#define pob          pop_back
+
+#define F            first
+#define S            second
+#define deb(x)       cout << #x << "=" << x << endl
+#define deb2(x, y)   cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define Fin          freopen("input.txt","r",stdin);
+#define Fout         freopen("output.txt","w",stdout);
+#define FasterIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define for0(i,n) for (int i = 0; i < (int)(n); ++i)
+
+template <typename T> void Print(T ar[] , int n) {for (int i = 0; i + 1 < n; i++)cout << ar[i] << " ";cout << ar[n - 1] << "\n";}
+template <typename T> void Print(const vector<T> &v) {for (int i = 0; i + 1 < v.size() ; i++) cout << v[i] << " ";cout << v.back() << "\n";}
+
+void solve(int n){
+    int i,j;
+    ll G=0;
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            G+=__gcd(i,j);
+        }
+    }
+    cout<<G<<endl;
+
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
+    
+    int n;
+    while(cin>>n && n!=0){
+        solve(n);
+    }
+    return 0;
+}
