dc3037f787806bee3547edfc49c02ade0cac6128
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-07-02 23:27:13 +0600

    Create primeFactorization.cpp

---
+++ template/NumberTheory/primeFactorization.cpp
@@ -0,0 +1,16 @@
+
+//O(sqrt(N))
+void primeFact(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            int cnt=0;
+            while(n%i==0){
+                cnt++;
+                n/=i;
+            }
+            cout<<i<< "^" <<cnt<<'\n';
+        }
+    }
+    if(n>1)
+        cout<<n<<"^"<<1<<'\n';
+}
