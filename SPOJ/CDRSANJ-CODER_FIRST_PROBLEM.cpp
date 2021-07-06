ff839b6d7ef33fbd597edc180dd1e5909fe46169
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-07-06 10:54:36 +0600

    Create CDRSANJ-CODER_FIRST_PROBLEM.cpp

---
+++ SPOJ/CDRSANJ-CODER_FIRST_PROBLEM.cpp
@@ -0,0 +1,85 @@
+/***
+**    Author: Rejaul Karim Ridhon
+***/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll           long long
+ 
+#define pb           push_back
+#define pob          pop_back
+ 
+#define F            first
+#define S            second
+#define deb(x)       cout << #x << "=" << x << endl
+#define deb2(x, y)   cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define Fin          freopen("input.txt","r",stdin);
+#define Fout         freopen("output.txt","w",stdout);
+#define FasterIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define for0(n) for (int i = 0; i < (int)(n); ++i)
+ 
+template <typename T> void Print(T ar[] , int n) {for (int i = 0; i + 1 < n; i++)cout << ar[i] << " ";cout << ar[n - 1] << "\n";}
+template <typename T> void Print(const vector<T> &v) {for (int i = 0; i + 1 < v.size() ; i++) cout << v[i] << " ";cout << v.back() << "\n";}
+ 
+int factSum(int n){
+    int sum=0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+ 
+            while(n%i==0){
+                if(i==2) sum+=2;
+                n/=i;
+            }
+        }
+    }
+    if(n>1){
+        if(n==2) sum+=2;
+    }
+    return sum;    
+}
+ 
+bool marked[100002];
+bool isPrime(int n){
+    if(n<2)  return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+    return marked[n]==false; 
+}
+void sieve(int n){
+    for(int i=3;i*i<=n;i+=2){
+        if(marked[i]==false){
+            for(int j=i*i;j<=n;j+=i+i){
+                marked[j]=true;
+            }
+        }
+    }
+}
+void solve(){
+    sieve(100002);
+    int n;
+    cin>>n;
+    if(n==1) cout<<1;
+    else if(n==2) cout<<2;
+    else if(isPrime(n)) cout<<0;
+    else cout<<factSum(n);
+ 
+    cout<<'\n';
+ 
+ 
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
+    
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        //cout << "Case #" << t  << ": ";
+        solve();
+    }
+    return 0;
+} 
