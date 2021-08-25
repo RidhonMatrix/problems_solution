80a2a904b71c3e5b55de49362fe160d16f6023aa
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2021-08-25 15:32:32 +0600

    Create 10392-Factoring_Large_Numbers.cpp

---
+++ uva/10392-Factoring_Large_Numbers.cpp
@@ -0,0 +1,80 @@
+/***
+**    Author: Rejaul Karim Ridhon
+***/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll           long long
+
+#define pb           push_back
+#define pob          pop_back
+
+/**Define memory set function**/
+#define mem(x,y) memset(x,y,sizeof(x))
+#define CLEAR(x) memset(x,0,sizeof(x))
+
+#define F            first
+#define S            second
+#define deb(x)       cout << #x << "=" << x << endl
+#define deb2(x, y)   cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define Fin          freopen("input.txt","r",stdin);
+#define Fout         freopen("output.txt","w",stdout);
+#define FasterIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define for0(n) for (int i = 0; i < (int)(n); ++i)
+
+template<class T> T MIN3(T a,T b,T c) {return min(a,min(b,c));} /// minimum of 3 number
+template<class T> T MAX3(T a,T b,T c) {return max(a,max(b,c));} ///maximum of 3 number
+template <typename T> void Print(T ar[] , int n) {for (int i = 0; i + 1 < n; i++)cout << ar[i] << " ";cout << ar[n - 1] << "\n";}
+template <typename T> void Print(const vector<T> &v) {for (int i = 0; i + 1 < v.size() ; i++) cout << v[i] << " ";cout << v.back() << "\n";}
+
+const int N=1e7;
+
+bool marked[N];
+bool isPrime(int n){
+    if(n<2) return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+    return marked[n]==false;
+}
+vector<ll>prime;
+void sieve(int n){
+    for(int i=3;i*i<=N;i+=2){
+        if(marked[i]==false){
+            for(int j=i*i;j<=n;j+=i+i){
+                marked[j]=true;
+            }
+        }
+    }
+
+}
+
+void solve(ll n){
+    for(int i=0;i<prime.size() && prime[i]*prime[i]<=n;i++){
+
+        while(n%prime[i]==0){
+            cout<<"    "<<prime[i]<<'\n';
+            n/=prime[i];
+        }
+    }
+    if(n>1) cout<<"    "<<n<<'\n';
+    cout<<'\n';
+}
+int main()
+{
+//    #ifndef ONLINE_JUDGE
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+//    #endif
+
+    sieve(N);
+    for(int i=2;i<=N;i++){
+        if(isPrime(i)) prime.pb(i);
+    }
+    ll n;
+    while(cin>>n && n>=0){
+        solve(n);
+    }
+    return 0;
+}
+
